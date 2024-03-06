4530a40ef68cc4cc797ebfb8eb0ed2ff165ea99c
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-03-06 11:26:06 +0000

    初期設定　login logout TOP about作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  namespace :user do
+    root to: "homes#top"
+    get 'homes/about'
+  end
+  # 顧客用
+  # url/users/sign_in
+  devise_for :users, controllers: {
+    registrations: "user/registrations",
+    sessions: 'user/sessions'
+  }
+  
+  #管理用
+  # url/admin/sign_up
+  devise_for :admin,  controllers: {
+    sessions: "admin/sessions"
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
