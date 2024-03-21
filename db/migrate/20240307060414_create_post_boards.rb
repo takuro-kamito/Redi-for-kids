d7ad7c3355923d2fe5a2d444b340da982ab6ceb2
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-03-21 12:43:47 +0000

    修正

--- db/migrate/20240307060414_create_post_boards.rb
+++ db/migrate/20240307060414_create_post_boards.rb
@@ -1,7 +1,7 @@
 class CreatePostBoards < ActiveRecord::Migration[6.1]
   def change
     create_table :post_boards do |t|
-      t.text :body, null: false, default: ""
+      t.text :body, null: false
 
       t.timestamps
     end
