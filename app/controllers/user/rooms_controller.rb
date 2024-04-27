7f15649764bc3c3bbdda0b5f303763b8d2dc2e86
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-04-27 10:18:30 +0000

    CSS修正

--- app/controllers/user/rooms_controller.rb
+++ app/controllers/user/rooms_controller.rb
@@ -5,7 +5,7 @@ class User::RoomsController < ApplicationController
     @room = Room.create #@room`という変数に、新しいチャットルームを作成。`Room.create`は、`Room`モデルに新しいレコードを作成するメソッド
     @current_entry = Entry.create(user_id: current_user.id, room_id: @room.id) #@current_entry`という変数に、現在のユーザー (`current_user`) のエントリーを作成
     @another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: @room.id) #Entry`モデルに新しいレコードを作成するメソッドです。このエントリーには、`user_id`として現在のユーザーのID (`current_user.id`) を、`room_id`として先ほど作成したチャットルームのID (`@room.id`) を指定
-    redirect_to user_rooms_path(@room) #@another_entry変数に、`params[:entry][:user_id]`で指定されたユーザーのエントリーを作成。`params[:entry][:user_id]`は、リクエストパラメータの`entry`ハッシュの`user_id`キーの値を取得しています。このエントリーには、`user_id`として指定されたユーザーのID、`room_id`として先ほど作成したチャットルームのIDを指定
+    redirect_to rooms_path(@room) #@another_entry変数に、`params[:entry][:user_id]`で指定されたユーザーのエントリーを作成。`params[:entry][:user_id]`は、リクエストパラメータの`entry`ハッシュの`user_id`キーの値を取得しています。このエントリーには、`user_id`として指定されたユーザーのID、`room_id`として先ほど作成したチャットルームのIDを指定
   end
 
 
